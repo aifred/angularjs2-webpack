e83bb246e7b0fe3d6cb8b3fd3e84dcab8c2a7a18
aifred <ong.shouquan@gmail.com>
2016-06-07 20:37:59 +0800

    Optimization of project dependencies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
 import { RouteConfig, ROUTER_DIRECTIVES, AsyncRoute } from '@angular/router-deprecated';
 
-import { DashboardComponent } from 'app/dashboard/dashboard.component';
-//import { BoilerplateComponent } from 'app/boilerplate/boilerplate.component';
+//import { DashboardComponent } from 'app/dashboard/dashboard.component';
 
 @Component({
   selector: 'my-app',
@@ -11,7 +10,14 @@ import { DashboardComponent } from 'app/dashboard/dashboard.component';
   directives: [ROUTER_DIRECTIVES]
 })
 @RouteConfig([
-	{path: '/dashboard', name: 'Dashboard', component: DashboardComponent, useAsDefault:true},
+	//{path: '/dashboard', name: 'Dashboard', component: DashboardComponent, useAsDefault:true},
+	new AsyncRoute({
+		path: '/dashboard', 
+		name: 'Dashboard', 
+		loader:() => new Promise(resolve => {
+			(<any>require).ensure(['app/dashboard/dashboard.component'], require => resolve(require('app/dashboard/dashboard.component').DashboardComponent));
+		})
+	}),
 	//{path: '/boilerplate', name: 'Boilerplate', component: BoilerplateComponent},
 	new AsyncRoute({
 		path: '/boilerplate', 
